62fb24520e8972e36b10cdc6ff481e57d5bb7656
Yoni Lavi <yoni@lavi.fm>
2018-02-09 04:54:27 +0000

    simplified basicsocket even more

--- basicsocket.py
+++ basicsocket.py
@@ -3,7 +3,7 @@ import socket
 
 IP = '127.0.0.1'
 PORT = 1234
-MAXIMUM_QUEUE_SIZE = 5
+MAXIMUM_QUEUE_SIZE = 0
 
 serversocket = socket.socket()
 serversocket.bind((IP, PORT))
@@ -13,7 +13,5 @@ print("Hello, I'm waiting for connections")
 
 (clientsocket, client_ip_and_port) = serversocket.accept()
 clientsocket.send(b"Hi user, here's my response :)\n")
-clientsocket.close()
 
 print("Well, I've had enough, I'll quit now")
-serversocket.close()
