ee2b182eaab6133d4f7032e2bde4a4f5aa8039e4
Yoni Lavi <yoni@codeinstitute.net>
2018-05-17 11:00:19 +0100

    remove mention of a server

--- socketecho.py
+++ socketecho.py
@@ -6,14 +6,14 @@ PORT = 1234
 MAXIMUM_QUEUE_SIZE = 0
 BUFFER_SIZE = 2048
 
-serversocket = socket.socket()
-serversocket.bind((IP, PORT))
+listening_socket = socket.socket()
+listening_socket.bind((IP, PORT))
 
-serversocket.listen(MAXIMUM_QUEUE_SIZE)
-print("Hello, I'm waiting for connections")
+listening_socket.listen(MAXIMUM_QUEUE_SIZE)
+print("Hello, I'm waiting for a connection")
 
 
-(clientsocket, client_ip_and_port) = serversocket.accept()
+(clientsocket, client_ip_and_port) = listening_socket.accept()
 while True:
     client_message = clientsocket.recv(BUFFER_SIZE).decode()
     response = ("You said: %s" % client_message).encode()
